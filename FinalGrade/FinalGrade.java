36f8d652944f29908ab5a7e6311cbb9e7a588de5
Antoine <w0441631@nscc.ca>
2020-01-23 10:54:49 -0400

    FinalGrade Using

--- FinalGrade/FinalGrade.java
+++ FinalGrade/FinalGrade.java
@@ -7,34 +7,18 @@ public class FinalGrade {
     public static void main(String[] args) {
 
         // grade variables
-        float assignment = 0.0f;
+        float[] assignment = new float[6];
 
         Scanner s = new Scanner(System.in);
 
         // loop 6 times to get all assignment grades
         float finalGrade = 0.0f;
         float lowest = 100.0f;
-        int i = 0;
-        while (assignment >= 0.0f) {
-            System.out.print("Enter grade for assignment " + (i+1) + " (-1 to end): ");
-            assignment = s.nextFloat();
+        for (int i=0; i<6; i++) {
+            System.out.print("Enter grade for assignment " + (i+1) + " : ");
+            assignment[i] = s.nextFloat();
 
-            if (assignment >= 0.0f ) {
-                finalGrade += assignment;
-
-                if (assignment < lowest) {
-                    lowest = assignment;
-                }
-            }
-
-            //i = i + 1;
-            //i += 1;
-            i++;
         }
-        // System.out.println("i = " + i); -- debug code
-        finalGrade -= lowest;
-        // subtracting 2 to account for lowest grade and -1 loop
-        finalGrade /= (i-2);
 
         System.out.println("Your final grade is: " + finalGrade);
     }
