686ff5d890a0c734023840b4c4bdacbc17d09394
Antoine Asanga <w0441631@nscc.ca>
2020-01-28 14:54:08 -0400

    ROT13 Cipher

---
+++ ROT13Cipher/ROT13Cipher.java
@@ -0,0 +1,21 @@
+package ROT13Cipher;
+
+im
+
+public class ROT13Cipher {
+
+    public static void main(String[] args) {
+
+        // ROT13 cipher is a very simple encryption cipher that rotates the alphabet by 13 letters to change
+        // "A" TO "N", "B" to "0", "C" to "P", "X" TO "K", "Y" to "L", "Z" to "M" etc
+
+        //e.g
+        //"secret" would be encrypted to "frperg"
+
+        //Create a program that takes a single word lowercase letters, stored as a constant variable, and outputs it as
+        //a ROT13 encrypted version.
+
+
+
+    }
+}
